b0da7581c2d01d301ea57dbadc66114f59a2df9b
Renan Fernandes <133548917+rehnanf@users.noreply.github.com>
2024-08-14 21:09:49 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,308 @@
+let nomeDoArquivo;
+
+function calcularTotal() {
+    const valorUnitario = document.querySelector("#valor_unitario").value;
+    const quantidade = document.querySelector("#quantidade").value;
+    const total = (valorUnitario * quantidade).toFixed(2);
+    document.querySelector("#valor_total").value = total;
+}
+
+function removerProduto(botao) {
+    const linha = botao.closest('tr');
+    linha.remove();
+    document.querySelector("#incProd").style.display = 'block';
+}
+
+function buscarEnderecoPorCep() {
+    const cepInput = document.querySelector('#cep');
+    let cep = cepInput.value.replace("-", "");
+
+    const camposParaLimpar = ['endereco', 'bairro', 'municipio', 'estado'];
+    camposParaLimpar.forEach(campo => {
+        document.querySelector(`#${campo}`).value = '';
+    });
+
+    if (cep.length === 8) {
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                if (!data.erro) {
+                    cepInput.value = data.cep;
+                    document.querySelector('#endereco').value = data.logradouro;
+                    document.querySelector('#bairro').value = data.bairro;
+                    document.querySelector('#municipio').value = data.localidade;
+                    document.querySelector('#estado').value = data.uf;
+                } else {
+                    alert('CEP não encontrado! Insira um CEP válido.');
+                }
+            });
+    } else {
+        alert('CEP inválido!!! O CEP deve ter pelo menos 8 dígitos.');
+    }
+}
+
+function adicionarProduto() {
+    const tabela = document.querySelector("#tabela_produtos tbody");
+    const novaLinha = document.createElement("tr");
+    novaLinha.classList.add("produto");
+
+    novaLinha.innerHTML = `
+        <td class="text-center align-middle">
+            <button onclick="removerProduto(this)" class="btn btn-danger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-trash3" viewBox="0 0 17 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"></path>
+                </svg>
+            </button>
+        </td>
+        <td>
+            <div class="border rounded border-dark p-2">
+                <h5>Produto 1</h5>
+                <div class="row mt-3 mb-3">
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-box-seam-fill" viewBox="0 0 16 16" style="width: 80px; height: 80px;">
+                            <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z"></path>
+                        </svg>
+                    </div>
+                    <div class="col-md-10">
+                        <label for="produto" class="text-sm">Produto</label>
+                        <input type="text" class="form-control" id="produto" name="produto" required>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="unidade_medida" style="font-size: 13px">UND. Medida</label>
+                                <select class="form-control" id="unidade_medida" name="unidade_medida" required>
+                                    <option value="" selected></option>
+                                    <option value="KG">KG</option>
+                                    <option value="Unidade">Unidade</option>
+                                    <option value="Caixa">Caixa</option>
+                                    <option value="Pacote">Pacote</option>
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="quantidade" style="font-size: 13px">QTDE em Estoque</label>
+                                <input type="number" class="form-control" id="quantidade" name="quantidade" min="0" required oninput="calcularTotal()">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="valor_unitario" style="font-size: 13px">Valor Unitário</label>
+                                <input type="number" class="form-control" id="valor_unitario" name="valor_unitario" min="0" step="0.01" required oninput="calcularTotal()">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="valor_total" style="font-size: 13px">Valor Total</label>
+                                <input type="number" class="form-control" id="valor_total" name="valor_total" readonly required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </td>`;
+    
+    tabela.appendChild(novaLinha);
+}
+
+function incluirAnexo() {
+    const input = document.querySelector('#documentoInput');
+    const arquivo = input.files[0];
+
+    if (arquivo) {
+        nomeDoArquivo = arquivo.name;
+        const blob = new Blob([arquivo], { type: arquivo.type });
+        const blobURL = URL.createObjectURL(blob);
+
+        sessionStorage.setItem('documentoBlobURL', blobURL);
+        alert('Documento anexado com sucesso!!!');
+    } else {
+        alert('Por favor, selecione um documento para anexar!!!');
+        return;
+    }
+
+    const tabelaAnexos = document.querySelector("#tabela_anexos tbody");
+    const novaLinha = document.createElement("tr");
+    novaLinha.classList.add("anexo");
+
+    novaLinha.innerHTML = `
+        <td>
+            <div class="border rounded border-dark p-2">
+                <h5>Itens</h5>
+                <div class="row mt-3 mb-3">
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <button onclick="excluirAnexo(this)" class="btn btn-danger">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-trash3" viewBox="0 0 17 16">
+                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="col-md-10">
+                        <div>
+                            <h6>${nomeDoArquivo}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </td>`;
+    
+    tabelaAnexos.appendChild(novaLinha);
+}
+
+function validarCamposObrigatorios() {
+    var camposObrigatorios = document.querySelectorAll('[required]');
+    var camposNaoPreenchidos = [];
+    var produtoAdicionado = document.querySelectorAll('#tabela_produtos .produto').length > 0;
+    var anexoAdicionado = document.querySelectorAll('#tabela_anexos .anexo').length > 0;
+
+    camposObrigatorios.forEach(function(campo) {
+        if (!campo.value.trim()) {
+            camposNaoPreenchidos.push(campo);
+            campo.style.borderColor = 'red';
+        } else {
+            campo.style.borderColor = '';
+        }
+    });
+
+    if (!produtoAdicionado) {
+        alert('ATENÇÃO!!! Pelo menos um produto deve ser adicionado.');
+        return false;
+    }
+
+    if (!anexoAdicionado) {
+        alert('ATENÇÃO!!! Pelo menos um documento deve ser anexado.');
+        return false;
+    }
+
+    if (camposNaoPreenchidos.length > 0) {
+        var camposNomes = camposNaoPreenchidos.map(function(campo) {
+            return campo.previousElementSibling.textContent.trim();
+        });
+
+        alert("Os seguintes campos são obrigatórios e não foram preenchidos:\n" + camposNomes.join('\n'));
+        return false;
+    }
+
+    return true;
+}
+
+function salvarFornecedor() {
+    if (!validarCamposObrigatorios()) {
+        return;
+    }
+
+    $('#loadingModal').modal('show');
+
+    var fornecedor = {
+        razaoSocial: document.getElementById('razao_social').value,
+        nomeFantasia: document.getElementById('nome_fantasia').value,
+        cnpj: document.getElementById('cnpj').value,
+        inscricaoEstadual: document.getElementById('inscricao_estadual').value,
+        inscricaoMunicipal: document.getElementById('inscricao_municipal').value,
+        cep: document.getElementById('cep').value,
+        endereco: document.getElementById('endereco').value,
+        numero: document.getElementById('numero').value,
+        complemento: document.getElementById('complemento').value,
+        bairro: document.getElementById('bairro').value,
+        municipio: document.getElementById('municipio').value,
+        estado: document.getElementById('estado').value,
+        contato: document.getElementById('contato').value,
+        telefone: document.getElementById('telefone').value,
+        email: document.getElementById('email').value,
+        produtos: Array.from(document.querySelectorAll('#tabela_produtos .produto')).map(function (row) {
+            return {
+                descricaoProduto: row.querySelector('#produto').value,
+                unidadeMedida: row.querySelector('#unidade_medida').value,
+                qtdeEstoque: row.querySelector('#quantidade').value,
+                valorUnitario: row.querySelector('#valor_unitario').value,
+                valorTotal: row.querySelector('#valor_total').value
+            };
+        }),
+        anexos: Array.from(document.querySelectorAll('#tabela_anexos .anexo')).map(function (row) {
+            return {
+                nomeArquivo: row.querySelector('label.text-sm').textContent,
+                blobArquivo: sessionStorage.getItem('documentoBlobURL_' + row.querySelector('label.text-sm').textContent) || ''
+            };
+        })
+    };
+
+    var dadosJSON = JSON.stringify(fornecedor, null, 2);
+
+    setTimeout(function () {
+        $('#loadingModal').modal('hide');
+        console.log('Dados JSON:', dadosJSON);
+
+        if (confirm('Os dados foram formatados em JSON. Deseja fazer o download do arquivo?')) {
+            var blob = new Blob([dadosJSON], { type: 'application/json' });
+            var url = URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = 'dados_fornecedor.json';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        }
+    }, 1000);
+}
+
+function incluirAnexo() {
+    var input = document.getElementById('documentoInput');
+    var file = input.files[0];
+    if (file) {
+        var nomearquivo = file.name;
+        var blob = new Blob([file], { type: file.type });
+        var blobURL = URL.createObjectURL(blob);
+
+        sessionStorage.setItem('documentoBlobURL_' + nomearquivo, blobURL);
+        alert('Seu documento foi anexado com sucesso!!!');
+
+        var novalinha = $("<tr>").addClass("anexo");
+        var linha = "";
+        linha += '    <td>';
+        linha += '        <div class="border rounded border-dark p-2">';
+        linha += '            <h5>Itens </h5>';
+        linha += '            <div class="row mt-3 mb-3">';
+        linha += '                <div class="col-md-1">';
+        linha += '                    <button onclick="excluirAnexo(this)" id="teste_2" class="btn btn-danger">';
+        linha += '                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-trash3" viewBox="0 0 17 16">';
+        linha += '                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zzm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 1 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"></path>';
+        linha += '                        </svg>';
+        linha += '                    </button>';
+        linha += '                </div>';
+        linha += '                <div class="col-md-1">';
+        linha += '                    <button onclick="visualizarDocumento(this)" class="btn btn-primary">';
+        linha += '                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="30" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">';
+        linha += '                            <path d="M16 8s-2.4-6-8-6S0 8 0 8s2.4 6 8 6 8-6 8-6zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path>';
+        linha += '                        </svg>';
+        linha += '                    </button>';
+        linha += '                </div>';
+        linha += '                <div class="col-md-10">';
+        linha += '                    <label class="text-sm">' + nomearquivo + '</label>';
+        linha += '                </div>';
+        linha += '            </div>';
+        linha += '        </div>';
+        linha += '    </td>';
+
+        novalinha.html(linha);
+        $('#tabela_anexos').append(novalinha);
+    }
+}
+
+function excluirAnexo(button) {
+    var nomeArquivo = $(button).closest('tr').find('label.text-sm').text();
+    sessionStorage.removeItem('documentoBlobURL_' + nomeArquivo);
+    $(button).closest('tr').remove();
+}
+
+function visualizarDocumento(button) {
+    var nomeArquivo = $(button).closest('tr').find('label.text-sm').text();
+    var blobURL = sessionStorage.getItem('documentoBlobURL_' + nomeArquivo);
+    if (blobURL) {
+        window.open(blobURL);
+    } else {
+        alert('Documento não encontrado.');
+    }
+}
+
+document.getElementById('btnSalvarFornecedor').addEventListener('click', salvarFornecedor);
+
+function excluirAnexo(botao) {
+    const linha = botao.closest('tr');
+    linha.remove();
+    sessionStorage.removeItem('documentoBlobURL');
+    alert('Anexo excluído com sucesso!');
+}
